1a00dbee776aec58ac26e7e4aa0f52a0dab40d1a
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-04-21 16:51:15 +0100

    added title and description for projects but a little messy so will amend code layout

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,35 @@
 import React from 'react';
 
+//Below are the styled-componenents imported
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {[{
+        title: 'Project 1',
+        description: "This is the description of the project"}, {
+          title: 'Project 1',
+        description: "This is the description of the project"}, {
+          title: "Project 1",
+        description: "This is the description of the project"}, {
+          title: "Project 1",
+        description: "This is the description of the project"}, {
+          title: "Project 1",
+        description: "This is the description of the project"
+      }].map((project) => (
+        <div>
+          {project.title}
+          <br />
+          {project.description}
+        </div>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
