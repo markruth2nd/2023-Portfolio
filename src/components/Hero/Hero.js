fab03fe5ef63a7dd25fe68d88f82c99e8e748062
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-04-21 16:26:18 +0100

    Added Title in Hero section as well as short description of self

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,21 @@
 import React from 'react';
-
+//The below are my styled-components, some in HeroStyles.js, some in styles Folder
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center> {/* main and center props have been applied her */}
+        Hello, This is <br />
+        Mark Baldry's Portfolio
+      </SectionTitle>
+      <SectionText>
+      A passionate and lively web developer, driven to create exceptional digital experiences through innovative and dynamic designs.
+      </SectionText>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
