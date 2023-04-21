37bad8e75354a336870cc1004f3ca3e7a4b724db
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-04-21 20:20:37 +0100

    added the carousel affect to the timeline for mobil responsiveness

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -47,7 +47,7 @@ const Timeline = () => {
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
       A passionate and lively web developer, driven to create exceptional digital experiences through innovative and dynamic designs.
-      </SectionText>
+      </SectionText> {/* The below carousel will be created for Mobile devises so that the user can scroll between the about me timeline if they so wish, it won'y be visible on desktop */}
       <CarouselContainer ref={carouselRef}>
         <>
         {TimeLineData.map((item, index) => (
@@ -58,11 +58,58 @@ const Timeline = () => {
             active={activeItem}
             onClick={(e) => handleClick(e, index)}
             >
-            </CarouselItem>
+              <CarouselItemTitle>
+                {item.year}
+                <CarouselItemImg
+                    width="208"
+                    height="6"
+                    viewBox="0 0 208 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M2.5 5.5C3.88071 5.5 5 4.38071 5 3V3.5L208 3.50002V2.50002L5 2.5V3C5 1.61929 3.88071 0.5 2.5 0.5C1.11929 0.5 0 1.61929 0 3C0 4.38071 1.11929 5.5 2.5 5.5Z"
+                      fill="url(#paint0_linear)"
+                      fill-opacity="0.33"
+                    />
+                    <defs>
+                      <linearGradient
+                        id="paint0_linear"
+                        x1="-4.30412e-10"
+                        y1="0.5"
+                        x2="208"
+                        y2="0.500295"
+                        gradientUnits="userSpaceOnUse">
+                        <stop stop-color="white" />
+                        <stop
+                          offset="0.79478"
+                          stop-color="white"
+                          stop-opacity="0"
+                        />
+                      </linearGradient>
+                    </defs>
+                  </CarouselItemImg>
+              </CarouselItemTitle>
+              <CarouselItemText>{item.text}</CarouselItemText>
+              </CarouselItem>
           </CarouselMobileScrollNode>
         ))}
         </>
       </CarouselContainer>
+      <CarouselButtons>
+        {TimeLineData.map((item, index) =>(
+          <CarouselButton
+          key={index}
+        index={index}
+        active={activeItem}
+        onClick={(e) => handleClick(e, index)}
+        type='button'
+      >
+        <CarouselButtonDot active={activeItem} />
+          </CarouselButton>
+        ))} 
+      </CarouselButtons>
     </Section>
   );
 };
