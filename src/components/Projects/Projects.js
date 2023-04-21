7690a9b26c4ac188f4ec1aaa298e55d28c430cb9
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-04-21 17:41:31 +0100

    added project images for projects section as well as stack tags ready to be amended

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,12 +11,23 @@ const Projects = () => (
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {projects.map((project) => (
-        <div>
-          {project.title}
-          <br />
-          {project.description}
-        </div>
+      {projects.map(({ id, image, title, description, tags, source, visit }) => (
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {tags.map((tag, i) => (
+                <Tag key={i}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+        </BlogCard>
       ))}
     </GridContainer>
   </Section>
