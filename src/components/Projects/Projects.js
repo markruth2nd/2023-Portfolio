87409e724808530b948c75d1da6fec11b2676bb3
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-04-21 17:01:37 +0100

    Projects info in constants.js so can be easier amended going forward

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,25 +5,13 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
-const projectsInfo = [{
-  title: 'Project 1',
-  description: "This is the description of the project"}, {
-    title: 'Project 2',
-  description: "This is the description of the project"}, {
-    title: "Project 3",
-  description: "This is the description of the project"}, {
-    title: "Project 4",
-  description: "This is the description of the project"}, {
-    title: "Project 5",
-  description: "This is the description of the project"
-}];
 
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {projectsInfo.map((project) => (
+      {projects.map((project) => (
         <div>
           {project.title}
           <br />
